b282390445d98701e71b34337d1e629b08899dd5
Rails Udemy Course Andauquer <geba23@hotmail.com>
2017-07-26 22:29:19 +0000

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-
+  #uniqueness, en conjunto con la validacion entre llaves, verifica que no exista otro nombre repetido
+  #en la base de datos, sin distinguir mayusculas y minusculas.
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, 
+             lenght: { minimum: 3, maximum: 25 }
+  #Esta es una expresion regular para validar el formato que deberia tener un email.
+  VALID_EMAIL_REGEX: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, 
+             lenght: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 end
