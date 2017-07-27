3a7ec6b26ebb8500b4e10b7a9cb57e71837ba3b2
Rails Udemy Course Andauquer <geba23@hotmail.com>
2017-07-27 18:50:48 +0000

    Create show action and add gravatar image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  #Segun la documentacion de Gravatar, esta es la manera en que vinculamos nuestro sitio web con su servicio.
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    #Recordemos que img_circle es una de las clases de bootstrap
+    image_tag(gravatar_url, alt: user.username, class: "img-circle")
+  end
 end
