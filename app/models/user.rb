51c6ffac8dff640c937aa163eccb0f1db229ea29
Rails Udemy Course Andauquer <geba23@hotmail.com>
2017-07-27 05:07:18 +0000

    Add association between user and articles and add debugger view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
+  has_many :articles
+  #Antes de guardar, convierte el email a solo minusculas.
+  before_save { self.email = email.downcase }
   #uniqueness, en conjunto con la validacion entre llaves, verifica que no exista otro nombre repetido
   #en la base de datos, sin distinguir mayusculas y minusculas.
   validates :username, presence: true, uniqueness: { case_sensitive: false }, 
-             lenght: { minimum: 3, maximum: 25 }
+             length: { minimum: 3, maximum: 25 }
   #Esta es una expresion regular para validar el formato que deberia tener un email.
-  VALID_EMAIL_REGEX: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, 
-             lenght: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
+             length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 end
