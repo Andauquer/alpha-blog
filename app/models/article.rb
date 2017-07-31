b7863400bb1c4263f1d91c01cc993bfa3efd095f
Rails Udemy Course Andauquer <geba23@hotmail.com>
2017-07-31 19:37:57 +0000

    Add article and categories association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  #Con las siguiente dos lineas se crea la relacion de muchos a muchos, del lado de articles.
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true, length: { minimum: 3, maximum: 50 } 
   validates :description, presence: true, length: { minimum: 10, maximum: 300 } 
   validates :user_id, presence: true
-end
+end 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729044844) do
+ActiveRecord::Schema.define(version: 20170731192710) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
